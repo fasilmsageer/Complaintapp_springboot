6ff4ebaa39c590a496ef7535e7b2ae58de6692d0
fasilmsageer <fasilmsageer@gmail.com>
2022-12-09 15:23:41 +0530

    userreg

---
+++ src/main/java/com/example/complaintreport_backend/dao/UserDao.java
@@ -0,0 +1,17 @@
+package com.example.complaintreport_backend.dao;
+
+
+import com.example.complaintreport_backend.model.UserDetails;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
+
+public interface UserDao extends CrudRepository<UserDetails,Integer> {
+    @Query(value = "SELECT `id`, `address`, `email`, `name`, `password`, `phone`, `username` FROM `userdetails` WHERE `username`= :username AND `password`= :password", nativeQuery = true)
+    List<UserDetails> UserLogin(@Param("username") String username, @Param("password") String password);
+
+    @Query(value = "SELECT `id`, `address`, `email`, `name`, `password`, `phone`, `username` FROM `userdetails` WHERE `username`= :username",nativeQuery = true)
+    List<UserDetails> FindUser(@Param("username") String username);
+}
